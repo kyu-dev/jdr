5a8382557991bb6a76539fde586824218e4d1e30
Arthur Vienot Ongaro <arthurvienot1008@gmail.com>
2025-05-07 14:50:02 +0200

    Ajout de la gestion des utilisateurs avec Supabase : création des pages de connexion et d'erreur, ainsi que des actions pour l'authentification. Remplacement de l'ancienne configuration Supabase par une nouvelle structure de fichiers pour le client et le serveur. Mise à jour des dépendances dans package.json et pnpm-lock.yaml.

--- app/page.js
+++ app/page.js
@@ -1,15 +1,3 @@
-import { supabase } from '@/lib/supabase';
-
 export default function Home() {
-  const setNewView = async () => {
-    const { data, error } = await supabase.from('view').insert({
-      name: 'arthur',
-    });
-    if (data) console.log(data);
-    if (error) console.log(error);
-  };
-
-  setNewView();
-
   return <div>Hello</div>;
 }
